eaa1e71342a6864e3e14e4afb99086d448691c17
Carlos Gonzales <carlos.rgn@gmail.com>
2017-02-06 21:29:12 -0600

    using typescript

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+const map = new Map<HTMLElement, EventListenerOrEventListenerObject>()
+
+export function install(Vue) {
+  Vue.directive('prevent-parent-scroll', {
+    bind(el: HTMLElement) {
+      const handler = (event: WheelEvent) => {
+        if (el.scrollTop === 0  && event.deltaY < 0 ||
+            el.scrollTop === el.scrollHeight - el.clientHeight &&
+            event.deltaY > 0)
+        {
+          event.preventDefault()
+        }
+      }
+      map.set(el, handler)
+      el.addEventListener('wheel', handler)
+    },
+    unbind(el: HTMLElement) {
+      el.removeEventListener('wheel', map.get(el))
+      map.delete(el)
+    }
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,25 @@ var path = require('path')
 
 module.exports = {
   devtool: 'source-map',
-  entry: './src/main.js',
+  entry: './src/main.ts',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'main.js',
     libraryTarget: 'umd'
   },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
     ]
-  }
+  },
+  plugins: [
+    require('webpack-fail-plugin')
+  ]
 }
