16c12b5f98c08628d60e29b387c976a12467d43e
zugende <zugende@gmail.com>
2018-02-08 21:23:13 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ export function install(Vue) {
   Vue.directive('prevent-parent-scroll', {
     bind(el: HTMLElement) {
       const handler = (event: WheelEvent) => {
-        if (el.scrollTop === 0  && event.deltaY < 0 ||
-            Math.abs(el.scrollTop - (el.scrollHeight - el.clientHeight)) <= 1 &&
+        if (el.scrollTop - Math.abs(event.deltaY) <= 0  && event.deltaY < 0 ||
+            Math.abs(el.scrollTop - (el.scrollHeight - el.clientHeight)) - event.deltaY <= 1 &&
             event.deltaY > 0)
         {
           event.preventDefault()
